7d6ffc456f429a5ec323b3b7d7958c8f3867224c
steve <ssaanen@gmail.com>
2016-05-25 20:57:14 +0200

    implemented new sms service which actually works (currently on test account) + sms service is now non-blocking

--- app/controller/Application.scala
+++ app/controller/Application.scala
@@ -9,6 +9,9 @@ import play.api.libs.json.{JsError, JsResult, Json}
 import play.api.mvc._
 import service.SmsService
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 class Application @Inject() (uberDriverInfoDao: UberDriverInfoDao, driverInfoDao: DriverInfoDao, sms: SmsService) extends Controller {
 
   def index = Action {
@@ -47,7 +50,13 @@ class Application @Inject() (uberDriverInfoDao: UberDriverInfoDao, driverInfoDao
     )
   }
 
-  def sendSMS = Action {
-    Ok(sms.sendTestMessageTo("0031628302534"))
+  def sendSMS = Action.async { request =>
+    val maybeEventualString: Option[Future[String]] = request.getQueryString("phone")
+      .map(sms.sendTestMessageTo)
+
+    maybeEventualString match {
+      case Some(s) => s.map(Ok(_))
+      case None => Future.successful(NotFound("Could not sms, maybe number incorrect?"))
+    }
   }
 }
