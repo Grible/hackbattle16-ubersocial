ccb6e5ed1f2f593e1829c0815b1c5d73898d83fe
Lennart Nederstigt <lennart@grible.co>
2016-05-25 19:50:14 +0200

    fixed oauth + cleaned up auth controller

--- app/controller/UberAuthenticationController.scala
+++ app/controller/UberAuthenticationController.scala
@@ -11,44 +11,34 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class UberAuthenticationController @Inject() (ws: WSClient) extends Controller {
 
+  val clientSecret = "1w0-YUL5d4XMZAhY4yhJWX1G6dWg-YvWTAO3f5kX"
+  val clientId = "wrM46LuAXxFP4CqmeaOX4wlO66g0ZsMI"
+  val url = "https://login.uber.com/oauth/v2/token"
+
   def index = Action {
-    Ok(view.html.connect())
+    Ok(view.html.connect(clientId))
   }
 
-  def confirmAuth = Action { implicit request => {
+  def confirmAuth = Action.async { implicit request => {
       val possibleAuthorizationCode: Option[String] = request.getQueryString("code")
       possibleAuthorizationCode match {
         case Some(code: String) =>
-          retrieveUberAccessToken(code)
-          Ok("")
-        case None => NotFound("Oops!")
+          retrieveUberAccessToken(code).map(_ => Ok(""))
+        case None => Future.successful(NotFound("Oops!"))
       }
     }
   }
 
-  def retrieveUberAccessToken(authorizationCode: String) = {
-    val url = "https://login.uber.com/oauth/v2/token"
-    val clientId = "wrM46LuAXxFP4CqmeaOX4wlO66g0ZsMI"
-    val clientSecret = "1w0-YUL5d4XMZAhY4yhJWX1G6dWg-YvWTAO3f5kX"
-    val request: WSRequest = ws.url(url)
-
+  def retrieveUberAccessToken(authorizationCode: String): Future[WSResponse] = {
     val data = Map(
       "client_secret" -> Seq(clientSecret),
       "client_id" -> Seq(clientId),
       "grant_type" -> Seq("authorization_code"),
-      "code" -> Seq(authorizationCode)
+      "code" -> Seq(authorizationCode),
+      "redirect_uri" -> Seq("http://localhost:9000/hookmeup")
     )
 
-    val futureResponse: Future[WSResponse] = request.post(data)
-
-    futureResponse.onSuccess { case res =>
-      println(res.json)
-      println(res.statusText)
-    }
-
-    futureResponse.onFailure { case res =>
-        println(res.getMessage)
-    }
+    ws.url(url).post(data)
   }
 
 }
